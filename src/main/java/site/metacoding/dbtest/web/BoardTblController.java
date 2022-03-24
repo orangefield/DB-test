c21499edecc352a3d71a8fbb5070997534858c46
field <jsw_777@naver.com>
2022-03-24 10:27:42 +0900

    6. 검색 기능 완료

--- src/main/java/site/metacoding/dbtest/web/BoardTblController.java
+++ src/main/java/site/metacoding/dbtest/web/BoardTblController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import site.metacoding.dbtest.domain.boardTbl.BoardTbl;
 import site.metacoding.dbtest.domain.boardTbl.BoardTblRepository;
@@ -19,9 +20,18 @@ public class BoardTblController {
     }
 
     @GetMapping("/search")
-    public String search(Model model) {
+    public String search(@RequestParam(defaultValue = "") String keyword, Model model) {
+        // keyword=Spring(RequestBody 적을 필요가 없다!)
 
-        List<BoardTbl> boards = boardTblRepository.findAll();
+        // if (keyword == null) {
+        // System.out.println("키워드 null");
+        // }
+        // if (keyword.equals("")) {
+        // System.out.println("키워드 공백");
+        // }
+
+        List<BoardTbl> boards = boardTblRepository.mSearch(keyword);
+        System.out.println("크기 : " + boards.size());
         model.addAttribute("boards", boards);
 
         return "home";
